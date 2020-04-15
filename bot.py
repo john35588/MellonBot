71462bd63d7aeeeea101a5722268197c62fcb8f5
john35588 <56525183+john35588@users.noreply.github.com>
2020-04-15 10:52:20 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ async def on_message(message):
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
     if "22jhoff" in message.author.name or "Plasmathrower" in message.author.name:
         await message.add_reaction("🌈")
-        await message.add_reaction("💖")
+        await message.add_reaction("💕")
         await message.add_reaction("😘")
         print("Response: Added reaction: 🌈")
     else:
