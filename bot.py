7c4958d15cac1dbbc3d9078a60980beef4473388
john35588 <56525183+john35588@users.noreply.github.com>
2020-04-14 22:31:07 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,18 +4,23 @@ token = "Njk5NjUwMDMwNDM2NjE0MTk0.XpZswg.I5UHOM-ItuF8nze07lzNbw2u0uo"
 
 client = discord.Client()  # starts the discord client.
 
-
-@client.event  # event decorator/wrapper. More on decorators here: https://pythonprogramming.net/decorators-intermediate-python-tutorial/
-async def on_ready():  # method expected by client. This runs once when connected
-    print(f'We have logged in as {client.user}')  # notification of login.
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
 @client.event
 async def on_message(message):
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
-    await message.add_reaction("🍉")
-    print("Response: Added reaction: 🍉")
+    if "22jhoff" in message.author.name:
+        await message.add_reaction("🌈")
+        print("Response: Added reaction: 🌈")
+    else:
+        await message.add_reaction("🍉")
+        print("Response: Added reaction: 🍉")
     
     if "hey melonbot" in message.content.lower() or "hi melonbot" in message.content.lower():
         await message.channel.send("Hey " + message.author.name + "!")
+    if
     
-client.run(token)  # recall my token was saved!
+    
+client.run(token)
