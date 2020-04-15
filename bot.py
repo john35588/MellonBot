cef831428ff1642fc741e5ae8b5ea6411c2df326
john35588 <56525183+john35588@users.noreply.github.com>
2020-04-14 19:27:25 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
 import discord
 from boto.s3.connection import S3Connection
-s3 = S3Connection(os.environ['S3_token'])
+token = S3Connection(os.environ['S3_token'])
 
 #print(discord.__version__)  # check to make sure at least once you're on the right version!
-
-token = process.env.token
 
 client = discord.Client()  # starts the discord client.
 
