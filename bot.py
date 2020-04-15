727dfa437ceba59d93498c80d3cbc19a78b21fe3
john35588 <56525183+john35588@users.noreply.github.com>
2020-04-14 22:08:22 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,6 @@ async def on_ready():  # method expected by client. This runs once when connecte
 async def on_message(message):
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
     await message.add_reaction("🍉")
-    print("Added reaction: 🍉")
+    print(f"Added reaction: 🍉")
 
 client.run(token)  # recall my token was saved!
