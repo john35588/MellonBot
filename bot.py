97c519a4d30d6f3854b05547ebf1f5d0607e4988
john35588 <56525183+john35588@users.noreply.github.com>
2020-04-18 21:09:22 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
-import discord
-from dadjokes import Dadjoke
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-import re
+import discord # Connects bot to discord
+from dadjokes import Dadjoke # Gets random Dad Jokes
+from urllib.request import urlopen # Used for getting XKCD images
+from bs4 import BeautifulSoup # also for getting XKCD images
+import re # Compiles images from XKCD for later access
 
+# Function to replace lines in the vars.txt file
 def replace_line(line_num, text):
     lines = open(vars.txt, 'r').readlines()
     lines[line_num] = text
@@ -17,19 +18,26 @@ client = discord.Client()
 t = True
 f = False
 
+# When the bot is connected
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+# When a message is sent to any channel
 @client.event
 async def on_message(message):
+	
+	# Print: ....Channel.............Author.........Author Username..........Message.......
     print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
+	
+	# Toggles reactions
     if "$react" in message.content.lower():
         if vars.readline([1]):
             replace_line(1, "f")
         else:
             replace_line(1, "t")
-            
+    
+	# Message Reactions
     if vars.readline([1]) or "Wally810" in message.author.name:
         if "22jhoff" in message.author.name or "Plasmathrower" in message.author.name:
             await message.add_reaction("🌈")
@@ -41,15 +49,18 @@ async def on_message(message):
         else:
             await message.add_reaction("🍉")
             print("Response: Added reaction: 🍉")
-		
+	
+	# Hey MelonBot response
     if "hey melonbot" in message.content.lower() or "hi melonbot" in message.content.lower():
         await message.channel.send("Hey " + message.author.name + "!")
     
+	# Gets random dad joke
     if "$joke" in message.content.lower():
         dadjoke = Dadjoke()
         joke = dadjoke.joke
         await message.channel.send(joke)
-
+	
+	# Gets XKCD comic
     if "$xkcd" in message.content.lower():
         request = message.content[6:]
         html = urlopen('https://xkcd.com/' + request + '/')
@@ -59,13 +70,16 @@ async def on_message(message):
         image = image[2:]
         print(image)
         await message.channel.send("http://" + image)
-		
+
+	# Deletes MelonMan's messages
 #    if "MelonManTakeMeByTheHand" in message.author.name:
 #        await message.delete()
-        
+
+# Makes bot's messages undeletable
 #@client.event
 #async def on_message_delete(message):
 #    if "MelonBot" in message.author.name:
 #        await message.channel.send(message.content)
 
+# Checks token authenticity with discord
 client.run(token)
