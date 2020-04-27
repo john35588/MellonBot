f54300ec41c64a1865a33a7c0de3376af208bfde
john35588 <56525183+john35588@users.noreply.github.com>
2020-04-26 21:57:52 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ client = commands.Bot(command_prefix = '$')
 
 @client.command(pass_context = True)
 async def clear(ctx, amount = 5):
-    if ctx.message.author.name == Wally810 or ctx.message.author.name == john35588:
+    if "Wally810" in ctx.message.author.name or "john35588" in ctx.message.author.name:
         channel = ctx.message.channel
         messages = []
         async for message in channel.history(limit=int(amount) + 1):
